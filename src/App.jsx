289a5616207cddcc1983dd64219b65a41b116cdf
Theo van Drimmelen <theo@vandrimmelen.eu>
2024-11-29 23:17:41 +0100

    Updated formatting

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   const [playersOfTheMatch, setPlayersOfTheMatch] = useState([]);
 
-  async function fetchPotm () {
+  async function fetchPotm() {
     const response = await fetch('https://fa-potm-67df3.azurewebsites.net/api/tallies');
     if (response.ok) {
       const data = await response.json();
@@ -26,18 +26,30 @@ function App() {
 
   const eachPlayerItem = useCallback((player, index) => {
     return (
-      <span>
-        <span>{index + 1}. </span>
-        <b>{player.id}</b> met {player.votes} stemmen
-        <br />
-      </span>
+      <tr>
+        <td>{index + 1}. </td>
+        <td>{player.id}</td>
+        <td>{player.votes}</td>
+      </tr>
     );
   }, [playersOfTheMatch]);
 
   return (
     <>
-      <h2>Player of the match</h2>
-      {playersOfTheMatch.map(eachPlayerItem)}
+      <div class="background"></div>
+      <div class="content">
+
+        <h2>Player of the match</h2>
+
+        <table align='center' border='thin'>
+          <tr>
+            <th>Plaats</th>
+            <th>Naam</th>
+            <th>Aantal stemmen</th>
+          </tr>
+          {playersOfTheMatch.map(eachPlayerItem)}
+        </table>
+      </div>
     </>
   )
 }
